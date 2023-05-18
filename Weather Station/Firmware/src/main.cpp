060ad38adabfa4649b81d4257fac6b38949c82a4
Yuliia Rudevych <49637429+YuliiaRudevych@users.noreply.github.com>
2023-05-18 17:29:57 +0300

    Update main.cpp

--- Weather Station/Firmware/src/main.cpp
+++ Weather Station/Firmware/src/main.cpp
@@ -1,5 +1,21 @@
 #include <Arduino.h>
 
+//Connecting sensors
+// BME
+// 1 VCC -> 3.3V
+// 2 GND -> GND
+// 3 SCL -> 18
+// 4 SDA -> 23
+// 5 CSB -> 5
+// 6 SDO -> 19
+
+
+// DHT
+// RED    -> 3.3V
+// BLACK  -> GND
+// YELLOW -> 25
+
+//Adding the required libraries
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
@@ -8,33 +24,25 @@
 #include <BlynkSimpleEsp32.h>
 #include <DHT.h>
 
-
-
-//COMPONENTS SETUP
-
 // BLYNK
 #define BLYNK_PRINT Serial
 
-#define BLYNK_TEMPLATE_ID "TMPL8DsBjx-P2"
-#define BLYNK_TEMPLATE_NAME "Weather Station TMPL8DsBjxP2"
-#define BLYNK_AUTH_TOKEN "91Ct3DnfdmmtLLkDvCXxyi9Kc505Hq9V"
+#define BLYNK_TEMPLATE_ID "*******"
+#define BLYNK_TEMPLATE_NAME "********"
+#define BLYNK_AUTH_TOKEN "*********"
 
-#define BLYNK_DOMAIN "..."
+#define BLYNK_DOMAIN "blynk.cloud"
 #define BLYNK_PORT 80 //443
 
-// BlynkTimer timer;
-
-// WIFI CONNECTION
-#define WIFI_SSID "..."
-#define WIFI_PASS "..."
 
 // DHT
 #define DHTPIN 25
-#define DHTTYPE DHT21  // AM2301
+#define DHTTYPE DHT21 
 
 #define DHT_BLYNK_VPIN_TEMPERATURE V0
 #define DHT_BLYNK_VPIN_HUMIDITY V1
 
+//DHT sensor settings
 DHT dht(DHTPIN, DHTTYPE);
 
 int DHT_ENABLED = 0;
@@ -43,12 +51,18 @@ float DHT_HUMIDITY_IGNORED_DELTA = 0.0001;
 float DHT_TEMPERATURE;
 float DHT_TEMPERATURE_IGNORED_DELTA = 0.0001;
 
+#define BMP_SCK  (18)
+#define BMP_MISO (19)
+#define BMP_MOSI (23)
+#define BMP_CS   (5)
+
 #define ALTITUDE_0 (1013.25)
 
 #define BMP_BLYNK_VPIN_PRESSURE V4
 #define BMP_BLYNK_VPIN_ALTITUDE V3
 
-Adafruit_BMP280 bmp;
+// BMP280 sensor settings;
+Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 int BMP_ENABLED = 0;
 float BMP_ALTITUDE;
@@ -59,13 +73,13 @@ float BMP_ALTITUDE_IGNORED_DELTA = 0.01;
 int RUN = 0;
 
 // SETUP BLOCK
-
+//Starting DHT method
 void setupDht() {
   Serial.println("DHT startup!");
   dht.begin();
   DHT_ENABLED = 1;
 }
-
+//Starting BMP method
 void setupBMP() {
   Wire.begin();
   unsigned status;
@@ -82,13 +96,13 @@ void setupBMP() {
     Serial.println(F("Valid BMP280 sensor"));
   }
 }
-
+//Sending data from DHT sensor to Blynk
 void sendDhtData() {
   Serial.println("Sending DHT data");
   Blynk.virtualWrite(DHT_BLYNK_VPIN_TEMPERATURE, DHT_TEMPERATURE);
   Blynk.virtualWrite(DHT_BLYNK_VPIN_HUMIDITY, DHT_HUMIDITY);
 }
-
+//Sending data from BMP sensor to Blynk
 void sendBMPData() {
   Serial.println("Sending BMP data");
   Blynk.virtualWrite(BMP_BLYNK_VPIN_PRESSURE, BMP_PRESSURE);
@@ -96,7 +110,7 @@ void sendBMPData() {
 }
 
 // DATA PROCESSING BLOCK
-
+//Reading DHT data
 void readAndSendDhtData() {
   float humidity = dht.readHumidity();
   float temperature = dht.readTemperature();
@@ -114,7 +128,7 @@ void readAndSendDhtData() {
     }
   }
 }
-
+//Reading BMP data
 void readBMPData() {
   float pressure = bmp.readPressure() / 100.0F;
   float altitude = bmp.readAltitude(ALTITUDE_0);
@@ -131,17 +145,16 @@ void readBMPData() {
   }
   delay(500);
 }
-
+//Setup block
 void setup() {
   Serial.begin(115200);
   setupDht();
   setupBMP();
   Serial.println("Blynk setup start");
-
   Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASS, BLYNK_DOMAIN, BLYNK_PORT);
   Serial.println("Setup is finished");
 }
-
+//Loop block
 void loop() {
   Blynk.run();  
   readAndSendDhtData();
