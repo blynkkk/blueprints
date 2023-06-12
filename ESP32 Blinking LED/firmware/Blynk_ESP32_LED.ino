88896792b251c7cf7bedb2df6d0f1cf918347e6a
Blynk IoT platform <pavel@blynk.cc>
2023-06-12 18:04:07 -0400

    Update Blynk_ESP32_LED.ino

--- ESP32 Blinking LED/firmware/Blynk_ESP32_LED.ino
+++ ESP32 Blinking LED/firmware/Blynk_ESP32_LED.ino
@@ -1,17 +1,22 @@
+// *** MAIN SETTINGS ***
+// Replace this block with correct template settings [HTTPS LINK TO Console/Templates/TemplateID]
+
 #define BLYNK_TEMPLATE_ID "TMPLxxxxxx"
 #define BLYNK_TEMPLATE_NAME "Device"
 #define BLYNK_AUTH_TOKEN "YourAuthToken"
 char ssid[] = "YourNetworkName";  // Your WiFi credentials.
 char pass[] = "YourPassword";     // Set password to "" for open networks.
 
-// Comment this out to disable prints
-#define BLYNK_PRINT Serial
+// *********************
+
+
 
 //Adding the required libraries
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
 
+#define BLYNK_PRINT Serial // Comment this out to disable serial monitor prints
 #define LED_PIN 2 // LED is usually connected to D2 pin. Change if needed.
 
 void setup()
@@ -26,7 +31,6 @@ void loop()
 {
   Blynk.run(); // Blynk magic happens here
 }
-
 
 
 BLYNK_WRITE(V0) // V0 is a datastream used to transfer and store LED switch state
