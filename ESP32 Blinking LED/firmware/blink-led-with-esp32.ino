d96ccbe44162a7619ace26fac8cb5d7403346021
Vitaliy <72790181+vveretko@users.noreply.github.com>
2023-06-14 09:27:19 +0300

    Add files via upload

---
+++ ESP32 Blinking LED/firmware/blink-led-with-esp32.ino
@@ -0,0 +1,52 @@
+
+// *** MAIN SETTINGS ***
+// Replace this block with correct template settings. You can find it for every template here:
+// https://blynk.cloud/dashboard/templates
+
+#define BLYNK_TEMPLATE_ID "TMPL4hh40RjZv"
+#define BLYNK_TEMPLATE_NAME "Blink an LED with ESP32"
+#define BLYNK_AUTH_TOKEN "Uo3Z9dCSjXCsO0DE-JywCiaRpRbizlSx"
+char ssid[] = "vveretko"; // Your WiFi network name
+char pass[] = "25252525"; // Your WiFi password. Set the password to "" for open networks.
+
+// *********************
+
+//Adding the required libraries
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+#define BLYNK_PRINT Serial  // Comment this out to disable serial monitor prints
+#define LED_PIN 2           // LED is usually connected to D2 pin. Change if needed.
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);                         // Make sure you have the same baud rate in your serial monitor set up
+ pinMode(LED_PIN, OUTPUT);                    // Setting LED PIN to control it
+  Blynk.begin(BLYNK_AUTH_TOKEN, ssid, pass);  // Starting Blynk on device with AuthToken and WiFi credentials
+}
+
+void loop()
+{
+  Blynk.run(); // Blynk magic happens here
+}
+
+BLYNK_WRITE(V0)
+// V0 is a datastream used to transfer and store LED switch state.
+// Evey time you use the LED switch in the app, this function
+// will listen and update the state on device  
+{
+  int value = param.asInt();
+  // Local variable `value` stores the incoming LED switch state (1 or 0)
+  // Based on this value, the physical LED on the board will be on or off:
+
+  if (value == 1)
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
