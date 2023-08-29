21afde0f4e3df2f3905b28e92aa35829f7731fb6
marynablynk <120122081+marynablynk@users.noreply.github.com>
2023-08-29 16:05:36 +0300

    Update particle_device_blueprint.ino

--- Particle Quickstart/Firmware/particle_device_blueprint.ino
+++ Particle Quickstart/Firmware/particle_device_blueprint.ino
@@ -1,10 +1,11 @@
 #include "Particle.h"
-
-const char* firmware_version = "0.0.0";
+#include <math.h>
+const char *firmware_version = "0.0.0";
 
 double v15 = 3.14159;
+uint32_t simSensor_timer_last = 0; // This is a variable for function simulates a sensor
 uint8_t led_state = LOW;
-bool particle_fn_called = TRUE;	// causes device to publish data immediately after started/boot and connected to Particle cloud.
+bool particle_fn_called = TRUE; // causes device to publish data immediately after started/boot and connected to Particle cloud.
 
 // Register the Particle cloud function
 int blynkLED(String on_or_off);
@@ -17,22 +18,40 @@ int blynkLED(String on_or_off);
 #define BLYNK_TEMPLATE_NAME "ParticleDeviceBlueprint"
 #define BLYNK_AUTH_TOKEN "your Blynk 32 char auth token"
 
+void simSensor()    //This is function simulates a sensor
+{ 
+  long sim = random(millis());
+  if (millis() - simSensor_timer_last >= 20000) {
+    simSensor_timer_last = millis();
+    float deltaSensor = cos(float(sim) / 1000) / 100;
+       if (v15 <= 0 || v15 >= 3.3)
+    {
+      v15 = v15 - deltaSensor;
+    }
+    else
+    {
+      v15 = v15 + deltaSensor;
+     }
+  }
+} // simSensor
+
 /////////////////////////////////////////////////////////////////////////
 
-
-bool deviceHasLedOnD7() {
+bool deviceHasLedOnD7()
+{
   // Returns TRUE if the device has a built-in LED on D7:
   //  Boron, Argon, Photon 2, Photon, Electron, Core
   // 8: P1
-  switch (PLATFORM_ID) {
-    case PLATFORM_BORON:
-    case PLATFORM_ARGON:
-    case 0:  // Core
-    case 6:  // Photon  (PLATFORM_PHOTON_PRODUCTION)
-    case 10: // Electron  (PLATFORM_ELECTRON_PRODUCTION)
-      return TRUE;
-    default:
-      return FALSE;
+  switch (PLATFORM_ID)
+  {
+  case PLATFORM_BORON:
+  case PLATFORM_ARGON:
+  case 0:  // Core
+  case 6:  // Photon  (PLATFORM_PHOTON_PRODUCTION)
+  case 10: // Electron  (PLATFORM_ELECTRON_PRODUCTION)
+    return TRUE;
+  default:
+    return FALSE;
   }
 } // deviceHasLedOnD7()
 
@@ -42,38 +61,47 @@ bool deviceHasLedOnD7() {
 const uint32_t TIMER_INTERVAL_MS = 300000L;
 uint32_t timer_last = 0;
 
-void pubToParticleBlynk() {
-  if (Particle.connected()) {
+void pubToParticleBlynk()
+{
+  if (Particle.connected())
+  {
+    
     char data[90]; // See serial output for the actual size in bytes and adjust accordingly.
-    // Note the escaped double quotes around the ""t"" for BLYNK_AUTH_TOKEN.  
+    // Note the escaped double quotes around the ""t"" for BLYNK_AUTH_TOKEN.
     snprintf(data, sizeof(data), "{\"t\":\"%s\",\"v14\":%u,\"v15\":%f,\"v16\":%u,\"v17\":%u}", BLYNK_AUTH_TOKEN, millis(), v15, led_state, led_state);
     Serial.printlnf("Sending to Blynk: '%s' with size of %u bytes", data, strlen(data));
     bool pub_result = Particle.publish("blynk_https_get", data, PRIVATE);
-    if (pub_result) {
+    if (pub_result)
+    {
       timer_last = millis();
-    } else {
+    }
+    else
+    {
       Serial.println("ERROR: Particle.publish()");
     }
   }
 } // pubToParticleBlynk()
 
-
-void pubTimer() {
+void pubTimer()
+{
   // A timer for publishing data to Particle Cloud, and then continuing to Blynk.
-  if (timer_last > millis())  timer_last = millis();
-  if ((millis() - timer_last) > TIMER_INTERVAL_MS && Particle.connected()) {
+  if (timer_last > millis())
+    timer_last = millis();
+  if ((millis() - timer_last) > TIMER_INTERVAL_MS && Particle.connected())
+  {
     pubToParticleBlynk();
     timer_last = millis();
   }
 } // pubTimer()
 
-
 /////////////////////////////////////////////////////////////////////////
 
+void setup()
+{
+  
 
-void setup() {
-
-  if (deviceHasLedOnD7() == TRUE) {
+  if (deviceHasLedOnD7() == TRUE)
+  {
     pinMode(D7, OUTPUT);
     digitalWrite(D7, LOW);
   }
@@ -92,47 +120,55 @@ void setup() {
 
 } // setup()
 
-
-
-void loop() {
-  
+void loop()
+{
+  simSensor(); // This is function simulates a sensor
   pubTimer();
+  
 
-  if (particle_fn_called == TRUE) {
+  if (particle_fn_called == TRUE)
+  {
     particle_fn_called = FALSE;
     // Publish data to Particle cloud..
     pubToParticleBlynk();
   }
-  
-  if (deviceHasLedOnD7() == TRUE) {
+
+  if (deviceHasLedOnD7() == TRUE)
+  {
     digitalWrite(D7, led_state);
-  } 
+  }
 
 } // loop()
 
-
-int blynkLED(String on_off) {
+int blynkLED(String on_off)
+{
   // Custom Particle cloud function that changes the state of the built-in LED
   // on D7 in response to an instruction from Blynk calling this
-  // custom cloud function. 
-  // Returns the value 1 if the LED has been turned on, and 0 if turned off, 
+  // custom cloud function.
+  // Returns the value 1 if the LED has been turned on, and 0 if turned off,
   // -1 if an unexpected on_off value is received.
   // Cloud functions must return int and take one String argument
   // curl https://api.particle.io/v1/devices/{your 25 char device id}/blynk_led
   // -d access_token={your 40 char access token}
   // -d "args=on/off"
-  
-  
-  if (on_off == "on" || on_off == "1") {
+
+  if (on_off == "on" || on_off == "1")
+  {
     particle_fn_called = TRUE;
     led_state = HIGH;
     return 1;
-  } else if (on_off == "off" || on_off == "0") {
+  }
+  else if (on_off == "off" || on_off == "0")
+  {
     particle_fn_called = TRUE;
     led_state = LOW;
     return 0;
-  } else {
-    Serial.print("Unexpected on_off value of: '"); Serial.print(on_off); Serial.println("'");
+  }
+  else
+  {
+    Serial.print("Unexpected on_off value of: '");
+    Serial.print(on_off);
+    Serial.println("'");
   }
   return -1;
 
