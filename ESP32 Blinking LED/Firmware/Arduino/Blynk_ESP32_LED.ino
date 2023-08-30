ba28fbcee9ceee0113e789c4ef911697fe039769
marynablynk <120122081+marynablynk@users.noreply.github.com>
2023-08-30 15:06:24 +0300

    Create Blynk_ESP32_LED.ino

---
+++ ESP32 Blinking LED/Firmware/Arduino/Blynk_ESP32_LED.ino
@@ -0,0 +1,57 @@
+
+// *** MAIN SETTINGS ***
+// Replace this block with correct template settings.
+// You can find it for every template here:
+//
+//   https://blynk.cloud/dashboard/templates
+
+#define BLYNK_TEMPLATE_ID     "TMPxxxxxx"
+#define BLYNK_TEMPLATE_NAME   "Device"
+#define BLYNK_AUTH_TOKEN      "YourAuthToken"
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+#define WIFI_SSID             "YourNetworkName"
+#define WIFI_PASS             "YourPassword"
+
+#define BLYNK_PRINT Serial  // Comment this out to disable serial monitor prints
+
+// Adding the required libraries
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <BlynkSimpleEsp32.h>
+
+#define LED_PIN 2           // LED is usually connected to D2 pin. Change if needed.
+
+BLYNK_WRITE(V0)
+// V0 is a datastream used to transfer and store LED switch state.
+// Evey time you use the LED switch in the app, this function
+// will listen and update the state on device  
+{
+  int value = param.asInt();
+  // Local variable `value` stores the incoming LED switch state (1 or 0)
+  // Based on this value, the physical LED on the board will be on or off:
+
+  if (value == 1)
+  {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  else
+  {
+    digitalWrite(LED_PIN, LOW);
+  }
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);                         // Make sure you have the same baud rate in your serial monitor set up
+  pinMode(LED_PIN, OUTPUT);                    // Setting LED PIN to control it
+
+  Blynk.begin(BLYNK_AUTH_TOKEN, WIFI_SSID, WIFI_PASS);
+}
+
+void loop()
+{
+  Blynk.run(); // Blynk magic happens here
+}
